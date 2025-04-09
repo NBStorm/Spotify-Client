198a6123fdafdb1b9359c777dd65e9df1daf43c3
NBStorm <94699501+NBStorm@users.noreply.github.com>
2025-04-09 23:35:53 +0700

    play-song

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,17 @@
-// import { useContext } from "react";
 import Player from "./components/Player";
 import Header from "./components/Header";
 import Layout from "./components/main-layout/Layout";
-// import { PlayerContext } from "./context/PlayerContext"
+import { PlayerContext } from "./context/PlayerContext";
+import { useContext } from "react";
 
 const App = () => {
-  // const {audioRef,track} = useContext(PlayerContext)
+  const { audioRef, track } = useContext(PlayerContext);
   return (
     <div className="h-screen bg-black">
       <Header />
       <Layout />
       <Player />
-      {/* <audio ref={audioRef} src={track.file} preload="auto"> */}
-
-      {/* </audio> */}
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
     </div>
   );
 };
---
+++ client/src/context/PlayerContext.jsx
@@ -0,0 +1,94 @@
+import { createContext, useEffect, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+  const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+
+  const [track, setTrack] = useState(songsData[0]);
+  const [playStatus, setPlayStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      seconds: 0,
+      minutes: 0,
+    },
+    totalTime: {
+      seconds: 0,
+      minutes: 0,
+    },
+  });
+
+  const [volume, setVolume] = useState(1); // 1 = 100%
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume;
+    }
+  }, [volume]);
+
+  const mute = () => {
+    audioRef.current.volume = 0;
+  }
+  const play = () => {
+    audioRef.current.play();
+    setPlayStatus(true);
+  };
+
+  const pause = () => {
+    audioRef.current.pause();
+    setPlayStatus(false);
+  };
+  const seekSong = async (e) => {
+    audioRef.current.currentTime =
+      (e.nativeEvent.offsetX / seekBg.current.offsetWidth) *
+      audioRef.current.duration;
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width =
+          Math.floor(
+            (audioRef.current.currentTime / audioRef.current.duration) * 100
+          ) + "%";
+        setTime({
+          currentTime: {
+            seconds: Math.floor(audioRef.current.currentTime % 60),
+            minutes: Math.floor(audioRef.current.currentTime / 60),
+          },
+          totalTime: {
+            seconds: Math.floor(audioRef.current.duration % 60),
+            minutes: Math.floor(audioRef.current.duration / 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
+
+  const contextValues = {
+    audioRef,
+    seekBg,
+    seekBar,
+    track,
+    setTrack,
+    playStatus,
+    setPlayStatus,
+    time,
+    setTime,
+    play,
+    pause,
+    seekSong,
+    volume,
+    setVolume,
+  };
+
+  return (
+    <PlayerContext.Provider value={contextValues}>
+      {props.children}
+    </PlayerContext.Provider>
+  );
+};
+
+export default PlayerContextProvider;
